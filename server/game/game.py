b28098004f7080f905f41195f74927a5784e946c
CodyCline <clinecody98@gmail.com>
2019-11-13 12:46:36 -0800

    Initial commit, project setup complete

---
+++ server/game/game.py
@@ -0,0 +1,10 @@
+
+class Game:
+    def __init__(self, board):
+        self.board = board
+
+    def server_turn(self, boardState):
+        pass
+
+    def check_winner(self, boardState):
+        pass
