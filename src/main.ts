f85daad14aa96f5873d8f8b016ba73ea41e31d6e
CodyCline <clinecody98@gmail.com>
2019-11-19 10:03:33 -0800

    frontend: added avatar cards to display turn

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 ***REMOVED***;
+// import VueSocketio from 'vue-socket.io';
 import App from './App.vue';
 import router from './router';
+
 
 Vue.config.productionTip = false;
 
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,25 +1,33 @@
 ***REMOVED***
-  <div class="gameBoard">
-		<TicTacSquare />
-		<TicTacSquare />
-		<TicTacSquare />
-		<TicTacSquare />
-		<TicTacSquare />
-		<TicTacSquare />
-		<TicTacSquare />
-		<TicTacSquare />
-		<TicTacSquare />
-  </div>
+	<div>
+		<div style="display: flex; flex-direction:row;align-items:center;">
+			<avatar/>
+			<avatar/>
+	***REMOVED***
+		<div class="gameBoard">
+			<TicTacSquare />
+			<TicTacSquare />
+			<TicTacSquare />
+			<TicTacSquare />
+			<TicTacSquare />
+			<TicTacSquare />
+			<TicTacSquare />
+			<TicTacSquare />
+			<TicTacSquare />
+	***REMOVED***
+***REMOVED***
 ***REMOVED***
 
 <script>
 import TicTacBoard from '@/components/TicTacBoard';
 import TicTacSquare from '@/components/TicTacSquare';
+import Avatar from '@/components/Avatar';
 
 export default {
 	components: {
 		TicTacBoard,
 		TicTacSquare,
+		Avatar,
 	***REMOVED***,
   	name: 'game',
 ***REMOVED***;
@@ -31,8 +39,6 @@ export default {
 	margin: 0 auto;
 	background-color: #664AC0;
 	border-radius: 10px;
-	
-
 	display: grid;
 	grid-template: repeat(3, 1fr) / repeat(3, 1fr);
 ***REMOVED***
